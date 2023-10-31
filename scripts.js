878424dbdee875a8b264c41167edb3672e85fe1d
bryanj408 <bryan.arndt52@gmail.com>
2023-10-31 08:14:00 -0700

    Created buttons and typingEffect function

--- scripts.js
+++ scripts.js
@@ -1,19 +1,35 @@
 const body = document.querySelector("body");
 
+//populates welcome message after initial load of browser
+//typingEffect() iterates through text string and uses setTimeout to pause between characters 
+
+const welcomeMessage = document.querySelector("div");
+const text = "Welcome to your new chatBuddy!";
+const typingEffect = (element, text, i = 0) => {
+    element.textContent += text[i];
+    
+    if (i === text.length - 1) {
+        return;
+    }
+    setTimeout(() => typingEffect(welcomeMessage, text, i + 1), 50);
+}
+typingEffect(welcomeMessage, text);
+
+
 let buttonOne = document.createElement("button");
 buttonOne.classList.add("buttonOne-class");
 buttonOne.setAttribute("id", "button");
-buttonOne.innerHTML = "Hello"
+buttonOne.innerHTML = "A1"
 body.append(buttonOne);
 
 let buttonTwo = document.createElement("button");
 buttonTwo.classList.add("buttonTwo-class");
 buttonTwo.setAttribute("id", "button");
-buttonTwo.innerHTML = "There"
+buttonTwo.innerHTML = "A2"
 body.append(buttonTwo);
 
 let buttonThree = document.createElement("button");
 buttonThree.classList.add("buttonThree-class");
 buttonThree.setAttribute("id", "button");
-buttonThree.innerHTML = "Guy"
+buttonThree.innerHTML = "A3"
 body.append(buttonThree);
