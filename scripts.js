94cc2bab26ccf94a9be1055dae99f9e0e39cdc25
bryanj408 <bryan.arndt52@gmail.com>
2023-11-01 21:30:56 -0700

    Created messageArray[] and setTimeouts() for messages

--- scripts.js
+++ scripts.js
@@ -3,30 +3,63 @@ CMD + / comments out line of code
 */
 
 const body = document.querySelector("body");
-const welcomeMessage = document.querySelector("div");
-const text = "Welcome to your new chatBuddy!";
-const text2 = "I want to know how you're feeling today, so I'm going to ask you a few question";
+const typedMessage = document.querySelector("div");
+const messageArray = [
+    "Welcome to your new chatBuddy!", 
+    "I want to ask you a few questions...", 
+    "Some of them might be a bit crazy.", 
+    "But, why not?",
+];
 
 /**
  * function typingEffect()
- * iterates through text constant to type out welcome message
- * @param {string} element 
- * @param {string} text 
+ * iterates through messageArray[] to type out messages
+ * setTimeout feature to iterate through characters/clear text
+ * @param {string} message 
+ * @param {string} item
  * @param {integer} i 
  * @returns 
  */
-const typingEffect = (element, text, i = 0) => {
-    element.textContent += text[i];
-    
-    if (i === text.length - 1) {
+
+const typingEffect = (message, item, i = 0) => {
+    message.textContent += item[i];
+    if (i === item.length - 1) {
         return;
     }
     setTimeout(() => { 
-        typingEffect(welcomeMessage, text, i + 1)
-    }, 50);
-   
+        typingEffect(message, item, i + 1)
+    }, 30);
 }
-typingEffect(welcomeMessage, text);
+
+/*
+    Calls typingEffect() and then goes through setTimeout() between messages.
+    I wanted to call setTimeout() individually to be able to better time the length
+    of each message vs. calling one setTimout within typingEffect() and repeating.
+*/
+typingEffect(typedMessage, messageArray[0]);
+setTimeout(() => {
+    typedMessage.textContent = "";
+}, 3000)
+setTimeout(() => {
+    typingEffect(typedMessage, messageArray[1]);
+}, 3100)
+setTimeout(() => {
+    typedMessage.textContent = "";
+}, 7000)
+setTimeout(() => {
+    typingEffect(typedMessage, messageArray[2]);
+}, 7100)
+setTimeout(() => {
+    typedMessage.textContent = "";
+}, 11000)
+setTimeout(() => {
+    typingEffect(typedMessage, messageArray[3]);
+}, 11100)
+setTimeout(() => {
+    typedMessage.textContent = "";
+}, 15000)
+
+
 
 
 let buttonOne = document.createElement("button");
